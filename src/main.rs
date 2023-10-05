ca254a65e6f12af627a0924d814943b98c12dcbf
myuwi <myuwi@proton.me>
2023-10-05 14:52:17 +0300

    feat: add graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use axum::Router;
 use std::net::SocketAddr;
+use tokio::signal::unix::{signal, SignalKind};
 
 mod web;
 
@@ -10,6 +11,19 @@ async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
+}
+
+async fn shutdown_signal() {
+    let mut term = signal(SignalKind::terminate()).expect("Failed to register SIGTERM handler");
+    let mut interrupt = signal(SignalKind::interrupt()).expect("Failed to register SIGINT handler");
+
+    tokio::select! {
+        _ = term.recv() => {},
+        _ = interrupt.recv() => {},
+    };
+
+    println!("Shutting down gracefully...");
 }
