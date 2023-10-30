a5823581f1cb5200bc59fa19dba905b4772679ad
myuwi <myuwi@proton.me>
2023-10-30 19:48:33 +0200

    feat: improve register error messages

--- src/web/register.rs
+++ src/web/register.rs
@@ -6,28 +6,66 @@ use uuid::Uuid;
 
 use crate::{model::user::UserAuth, web::AUTH_TOKEN};
 
-#[derive(Template)]
+#[derive(Default)]
+struct Errors {
+    username: Option<String>,
+    password: Option<String>,
+}
+
+#[derive(Template, Default)]
+#[template(path = "auth-fields.html")]
+pub struct AuthFields {
+    errors: Errors,
+}
+
+impl AuthFields {
+    fn with_username_error(msg: impl Into<String>) -> Self {
+        Self {
+            errors: Errors {
+                username: Some(msg.into()),
+                ..Default::default()
+            },
+        }
+    }
+
+    fn with_password_error(msg: impl Into<String>) -> Self {
+        Self {
+            errors: Errors {
+                password: Some(msg.into()),
+                ..Default::default()
+            },
+        }
+    }
+}
+
+#[derive(Template, Default)]
 #[template(path = "register.html")]
-struct Register {}
+struct Register {
+    errors: Errors,
+}
 
 pub async fn get() -> impl IntoResponse {
-    Register {}
+    Register::default()
 }
 
 pub async fn post(
     State(pool): State<SqlitePool>,
     cookies: Cookies,
     Form(form_data): Form<UserAuth>,
-) -> impl IntoResponse {
-    // TODO: Out of Band updates
+) -> Result<impl IntoResponse, AuthFields> {
+    // TODO: Better form validation
     if !(4..=32).contains(&form_data.username.len()) {
-        return "Username must be between 4 and 32 characters".into_response();
-    }
-    if !(6..=64).contains(&form_data.password.len()) {
-        return "Password must be between 6 and 64 characters".into_response();
+        return Err(AuthFields::with_username_error(
+            "Username must be between 4 and 32 characters",
+        ));
     }
 
-    // TODO: Validate form data
+    if !(6..=64).contains(&form_data.password.len()) {
+        return Err(AuthFields::with_password_error(
+            "Password must be between 6 and 64 characters",
+        ));
+    }
+
     let res = sqlx::query("INSERT INTO users (id, username, password) VALUES ($1, $2, $3)")
         .bind(Uuid::new_v4())
         .bind(&form_data.username)
@@ -36,26 +74,25 @@ pub async fn post(
         .execute(&pool)
         .await;
 
-    if let Err(err) = res {
+    res.map_err(|err| {
         // TODO: Better error messages
-        let msg = match err
+        match err
             .as_database_error()
             .map(|e| e.code().unwrap_or_default())
             .unwrap_or_default()
             .to_string()
             .as_str()
         {
-            "2067" => "Username is already taken",
-            _ => "Unable to create account",
-        };
+            "2067" => AuthFields::with_username_error("Username is already taken"),
+            _ => AuthFields::with_username_error("Unable to create account"),
+        }
+    })?;
 
-        return msg.into_response();
-    }
+    // TODO: Generate a real token
+    let auth_cookie = Cookie::build(AUTH_TOKEN, format!("{}.fake.token", &form_data.username))
+        .http_only(true)
+        .finish();
+    cookies.add(auth_cookie);
 
-    cookies.add(Cookie::new(
-        AUTH_TOKEN,
-        format!("{}.fake.token", &form_data.username),
-    ));
-
-    ([("HX-Redirect", "/")], "").into_response()
+    Ok([("HX-Redirect", "/")])
 }
