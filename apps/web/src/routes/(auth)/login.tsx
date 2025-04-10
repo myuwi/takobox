5b9966fb017b15264a25b60d8341de825b9ac106
myuwi <myuwi@proton.me>
2025-04-10 19:48:34 +0300

    style: reorder imports

--- apps/web/src/routes/(auth)/login.tsx
+++ apps/web/src/routes/(auth)/login.tsx
@@ -1,5 +1,5 @@
-import { useForm } from "react-hook-form";
 import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
+import { useForm } from "react-hook-form";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { Label } from "@/components/Label";
