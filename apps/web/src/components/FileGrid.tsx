7edce3238de20e952d493eb23da333e4de8e31b7
myuwi <myuwi@proton.me>
2025-08-06 16:07:07 +0300

    fix: fix selected files indicator placement

--- apps/web/src/components/FileGrid.tsx
+++ apps/web/src/components/FileGrid.tsx
@@ -195,95 +195,96 @@ export const FileGrid = ({ files }: FileGridProps) => {
   }, [setSelectedFiles]);
 
   return (
-    <div
-      className="group/grid relative grid h-full w-full grid-cols-[repeat(auto-fill,10rem)] content-start justify-around gap-2 overflow-auto p-2"
-      onClick={handleGridClick}
-      data-selecting={selectedFiles.length > 0}
-    >
-      {files.map((file) => {
-        const selected = selectedFiles.includes(file);
+    <div className="relative h-full w-full" onClick={handleGridClick}>
+      <div
+        className="group/grid grid h-full w-full grid-cols-[repeat(auto-fill,10rem)] content-start justify-around gap-2 overflow-auto p-2"
+        data-selecting={selectedFiles.length > 0}
+      >
+        {files.map((file) => {
+          const selected = selectedFiles.includes(file);
 
-        const handleOpen = () => window.open(`/${file.name}`);
+          const handleOpen = () => window.open(`/${file.name}`);
 
-        const handleSelect = (ctrlKey: boolean) => {
-          if (ctrlKey) {
-            if (selected) {
-              setSelectedFiles(selectedFiles.filter((f) => f !== file));
+          const handleSelect = (ctrlKey: boolean) => {
+            if (ctrlKey) {
+              if (selected) {
+                setSelectedFiles(selectedFiles.filter((f) => f !== file));
+              } else {
+                setSelectedFiles([...selectedFiles, file]);
+              }
             } else {
-              setSelectedFiles([...selectedFiles, file]);
+              setSelectedFiles([file]);
             }
-          } else {
-            setSelectedFiles([file]);
-          }
-        };
+          };
 
-        const handleClick = (e: MouseEvent<HTMLDivElement>) => {
-          e.stopPropagation();
-          handleSelect(e.ctrlKey);
-        };
+          const handleClick = (e: MouseEvent<HTMLDivElement>) => {
+            e.stopPropagation();
+            handleSelect(e.ctrlKey);
+          };
 
-        const handleDoubleClick = () => handleOpen();
+          const handleDoubleClick = () => handleOpen();
 
-        const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
-          e.stopPropagation();
-          switch (e.key) {
-            case " ":
-              handleSelect(e.ctrlKey);
-              break;
-            case "Enter":
-              handleOpen();
-              break;
-            case "Escape":
-              setSelectedFiles([]);
-              break;
-          }
-        };
+          const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+            e.stopPropagation();
+            switch (e.key) {
+              case " ":
+                handleSelect(e.ctrlKey);
+                break;
+              case "Enter":
+                handleOpen();
+                break;
+              case "Escape":
+                setSelectedFiles([]);
+                break;
+            }
+          };
 
-        const handleCheckboxClick = (e: MouseEvent<HTMLSpanElement>) => {
-          e.stopPropagation();
-          handleSelect(true);
-        };
+          const handleCheckboxClick = (e: MouseEvent<HTMLSpanElement>) => {
+            e.stopPropagation();
+            handleSelect(true);
+          };
 
-        const handleMenuOpen = () => setSelectedFiles([file]);
+          const handleMenuOpen = () => setSelectedFiles([file]);
 
-        return (
-          <div
-            key={file.id}
-            className="group flex h-min w-40 flex-col items-center rounded-md p-2 select-none hover:bg-accent/50 aria-selected:bg-accent"
-            role="gridcell"
-            tabIndex={0}
-            aria-selected={selected}
-            onClick={handleClick}
-            onDoubleClick={handleDoubleClick}
-            onKeyDown={handleKeyDown}
-          >
-            <div className="relative w-full">
-              <FileThumbnail file={file} />
-              <span
-                className="invisible absolute top-0 left-0 rounded-sm bg-accent p-0.5 inset-ring inset-ring-primary-foreground group-hover:visible group-aria-selected:visible group-aria-selected:bg-primary-foreground group-aria-selected:text-white group-data-[selecting=true]/grid:visible"
-                onClick={handleCheckboxClick}
-                onDoubleClick={stopPropagation}
-              >
-                <Check className="size-4 group-not-aria-selected:invisible" />
-              </span>
-              <ContextMenuDropdown file={file} onOpen={handleMenuOpen}>
-                <Button
-                  variant="accent"
-                  size="icon-sm"
-                  className="invisible absolute right-0 bottom-0 group-hover:visible group-aria-selected:visible"
-                  onClick={stopPropagation}
+          return (
+            <div
+              key={file.id}
+              className="group flex h-min w-40 flex-col items-center rounded-md p-2 select-none hover:bg-accent/50 aria-selected:bg-accent"
+              role="gridcell"
+              tabIndex={0}
+              aria-selected={selected}
+              onClick={handleClick}
+              onDoubleClick={handleDoubleClick}
+              onKeyDown={handleKeyDown}
+            >
+              <div className="relative w-full">
+                <FileThumbnail file={file} />
+                <span
+                  className="invisible absolute top-0 left-0 rounded-sm bg-accent p-0.5 inset-ring inset-ring-primary-foreground group-hover:visible group-aria-selected:visible group-aria-selected:bg-primary-foreground group-aria-selected:text-white group-data-[selecting=true]/grid:visible"
+                  onClick={handleCheckboxClick}
                   onDoubleClick={stopPropagation}
                 >
-                  <Ellipsis />
-                </Button>
-              </ContextMenuDropdown>
+                  <Check className="size-4 group-not-aria-selected:invisible" />
+                </span>
+                <ContextMenuDropdown file={file} onOpen={handleMenuOpen}>
+                  <Button
+                    variant="accent"
+                    size="icon-sm"
+                    className="invisible absolute right-0 bottom-0 group-hover:visible group-aria-selected:visible"
+                    onClick={stopPropagation}
+                    onDoubleClick={stopPropagation}
+                  >
+                    <Ellipsis />
+                  </Button>
+                </ContextMenuDropdown>
+              </div>
+              <span className="line-clamp-1 px-2 text-center break-all">
+                {file.original}
+              </span>
             </div>
-            <span className="line-clamp-1 px-2 text-center break-all">
-              {file.original}
-            </span>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
       <SelectedFilesIndicator files={selectedFiles} />
     </div>
   );
