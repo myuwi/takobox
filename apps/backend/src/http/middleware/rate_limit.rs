90c68cfefad328ba3c69a9907975e44cddeb0025
myuwi <myuwi@proton.me>
2025-10-06 15:45:46 +0300

    feat: rate limit requests based on the x-forwarded-for header

--- apps/backend/src/http/middleware/rate_limit.rs
+++ apps/backend/src/http/middleware/rate_limit.rs
@@ -1,21 +1,36 @@
+use std::net::IpAddr;
+
 use axum::{body::Body, response::IntoResponse};
 use governor::{clock::QuantaInstant, middleware::NoOpMiddleware};
 use tower_governor::{
-    GovernorError, GovernorLayer, governor::GovernorConfigBuilder,
-    key_extractor::SmartIpKeyExtractor,
+    GovernorError, GovernorLayer, governor::GovernorConfigBuilder, key_extractor::KeyExtractor,
 };
 
 use crate::http::error::Error;
 
-// TODO: Implement a custom key extractor with only "x-forwarded-for"
+#[derive(Clone, Debug)]
+pub struct IpKeyExtractor;
+
+impl KeyExtractor for IpKeyExtractor {
+    type Key = IpAddr;
+
+    fn extract<T>(&self, req: &axum::http::Request<T>) -> Result<Self::Key, GovernorError> {
+        req.headers()
+            .get("x-forwarded-for")
+            .and_then(|hv| hv.to_str().ok())
+            .and_then(|s| s.split(',').find_map(|s| s.trim().parse::<IpAddr>().ok()))
+            .ok_or(GovernorError::UnableToExtractKey)
+    }
+}
+
 pub fn rate_limit(
     replenish_ms: u64,
     burst_size: u32,
-) -> GovernorLayer<SmartIpKeyExtractor, NoOpMiddleware<QuantaInstant>, Body> {
+) -> GovernorLayer<IpKeyExtractor, NoOpMiddleware<QuantaInstant>, Body> {
     let governor_conf = GovernorConfigBuilder::default()
         .per_millisecond(replenish_ms)
         .burst_size(burst_size)
-        .key_extractor(SmartIpKeyExtractor)
+        .key_extractor(IpKeyExtractor)
         .finish()
         .unwrap();
 
