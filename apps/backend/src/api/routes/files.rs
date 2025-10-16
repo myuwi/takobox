43b5680321409b6c324b764b8af9bdf445bf56dd
myuwi <myuwi@proton.me>
2025-10-16 11:59:44 +0300

    feat: reduce default file id length to 6

--- apps/backend/src/api/routes/files.rs
+++ apps/backend/src/api/routes/files.rs
@@ -23,6 +23,13 @@ use crate::{
     types::Uuid,
 };
 
+pub const ALPHABET: [char; 62] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+    'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+    'V', 'W', 'X', 'Y', 'Z',
+];
+
 async fn index(
     State(AppState { pool, .. }): State<AppState>,
     session: Session,
@@ -90,7 +97,8 @@ async fn create(
     };
 
     // TODO: retry on db collision
-    let file_id = nanoid!(12);
+    // TODO: make length configurable?
+    let file_id = nanoid!(6, &ALPHABET);
     let ext = std::path::Path::new(&original_name)
         .extension()
         .and_then(OsStr::to_str)
